4eeae441fc88e811d31068a0acb0085919b51c5d
meysed <mey.sedighi@gmail.com>
2019-11-02 01:54:42 +0330

    first

---
+++ src/main/java/com/packet/sb5be/searchapp/Controller.java
@@ -0,0 +1,13 @@
+package com.packet.sb5be.searchapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello World !\n";
+    }
+}
